3aeb6d2877b8d2d61005448e60cca8e6902678d5
NovaSquirrel <bushytail@novasquirrel.com>
2019-11-22 20:10:31 -0500

    Swap out font and level graphics; fixes #4

--- chr/compress graphics.bat
+++ chr/compress graphics.bat
@@ -1,2 +1,5 @@
-python pb16.py nova.chr nova.pb16
+python pb16.py font.chr font.pb16
+python pb16.py level.chr level.pb16
+python pb16.py sprites.chr sprites.pb16
+python pb16.py shared.chr shared.pb16
 pause
